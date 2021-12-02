9116d097f420cd120a966563292c9998b0314c0c
Will Schenk <wschenk@gmail.com>
2021-12-02 16:57:48 -0500

    Fixed the workflow names and adding startup message

--- proxy.js
+++ proxy.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
+console.log( "Proxy starting up on port 3000" );
+
 const proxy = httpProxy.createProxyServer({});
 http.createServer(function(req, res) {
     console.log('Request', req.method, req.url);
