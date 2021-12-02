e70c3593386f98baeb8603ffb6b44d36725dd4de
Will Schenk <wschenk@gmail.com>
2021-12-02 17:14:01 -0500

    Trying something else

--- proxy.js
+++ proxy.js
@@ -1,11 +1,9 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-console.log( "Proxy starting up on port 3000" );
-console.log( "http://helloworld-go.default.svc.cluster.local" );
+const remote = "http://helloworld-go.default.svc.cluster.local";
 
-const proxy = httpProxy.createProxyServer({});
-http.createServer(function(req, res) {
-    console.log('Request', req.method, req.url);
-    proxy.web(req, res, { target: 'http://helloworld-go.default.svc.cluster.local' } );
-}).listen(3000);
+console.log( "Proxy starting up on port 3000" );
+console.log( `Proxing to ${remote}` );
+
+httpProxy.createProxyServer({target:remote}).listen(3000);
