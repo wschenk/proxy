0e1564d781af1748c37e96f1e75019a94ef93e37
Will Schenk <wschenk@gmail.com>
2021-12-07 17:29:14 -0500

    Renamed env variable to LISTENING_PORT

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 const remote = process.env.REMOTE_TARGET || "http://homepage.default.svc.cluster.local";
-const port = process.env.PORT || 3000;
+const port = process.env.LISTENING_PORT || 3000;
 
 console.log( `Proxy starting up on port ${port}` );
 console.log( `Proxing to ${remote}` );
